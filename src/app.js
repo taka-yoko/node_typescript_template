5ca11eef116901304ad07699f5d9bb700dfe0577
taka-yoko <*****@users.noreply.github.com>
2020-11-24 00:17:33 +0900

    simpleなserver作成

--- src/app.js
+++ src/app.js
@@ -1,2 +1,15 @@
-console.log('"hello node.js x typescript');
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const http = require("http");
+class Main {
+    constructor() {
+        const server = http.createServer((request, response) => this.requestHandler(request, response));
+        server.listen("5000");
+        console.log("http://localhost:5000 へアクセスしてください。");
+    }
+    requestHandler(request, response) {
+        response.end("Hello, node.js with TypeScript");
+    }
+}
+new Main();
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,23 @@
-console.log('"hello node.js x typescript');
+import * as http from 'http';
+
+class Main {
+  constructor() {
+    const server: http.Server = http.createServer(
+      (request: http.IncomingMessage, response: http.ServerResponse) =>
+        this.requestHandler(request, response)
+    );
+    server.listen("5000");
+
+    console.log("http://localhost:5000 へアクセスしてください。");
+  }
+
+  private requestHandler(
+    request: http.IncomingMessage,
+    response: http.ServerResponse
+  ): void {
+    response.end("Hello, node.js with TypeScript");
+  }
+}
+
+new Main();
+
